cb3c7b37a5ac47ce3f2ae0e379fe9bff8d3f24b7
megha77777 <meghacute786@gmail.com>
2024-06-24 21:17:17 +0530

    category resource

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
 class Category extends Model
 {
@@ -20,4 +21,13 @@ class Category extends Model
     {
         return $this->hasMany(Product::class);
     }
+
+    // protected static function boot()
+    // {
+    //     parent::boot();
+
+    //     static::creating(function ($question) {
+    //         $question->slug = Str::slug($question->name);
+    //     });
+    // }
 }
