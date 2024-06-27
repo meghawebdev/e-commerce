42d2de17bd740a261853cf1d71f12549d337833e
megha77777 <meghacute786@gmail.com>
2024-06-27 22:56:43 +0530

    tabs

--- app/Filament/Resources/OrderResource/Pages/ListOrders.php
+++ app/Filament/Resources/OrderResource/Pages/ListOrders.php
@@ -5,6 +5,7 @@ namespace App\Filament\Resources\OrderResource\Pages;
 use App\Filament\Resources\OrderResource;
 use App\Filament\Resources\OrderResource\Widgets\OrderStats;
 use Filament\Actions;
+use Filament\Resources\Components\Tab;
 use Filament\Resources\Pages\ListRecords;
 
 class ListOrders extends ListRecords
@@ -22,6 +23,19 @@ class ListOrders extends ListRecords
     {
         return [
             OrderStats::class,
+        ];
+    }
+
+    public function getTabs(): array
+    {
+        return [
+            null => Tab::make('All'),
+            // it will fetch all the orders whose status is new
+            'New' => Tab::make()->query(fn ($query) => $query->where('status', 'new')),
+            'Processing' => Tab::make()->query(fn ($query) => $query->where('status', 'processing')),
+            'Shipped' => Tab::make()->query(fn ($query) => $query->where('status', 'shipped')),
+            'Delivered' => Tab::make()->query(fn ($query) => $query->where('status', 'delivered')),
+            'Canceled' => Tab::make()->query(fn ($query) => $query->where('status', 'canceled')),
         ];
     }
 }
