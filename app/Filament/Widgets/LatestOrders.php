d1432fc110840196d97176e17f9e96b9b5e7700b
megha77777 <meghacute786@gmail.com>
2024-07-01 23:37:41 +0530

    dashboard table widget

---
+++ app/Filament/Widgets/LatestOrders.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Filament\Widgets;
+
+use App\Filament\Resources\OrderResource;
+use App\Models\Order;
+use Filament\Tables;
+use Filament\Tables\Actions\Action;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Table;
+use Filament\Widgets\TableWidget as BaseWidget;
+
+class LatestOrders extends BaseWidget
+{
+    protected int|array|string $columnSpan = 'full';
+
+    public function table(Table $table): Table
+    {
+        return $table
+            ->query(OrderResource::getEloquentQuery())
+            ->defaultPaginationPageOption(5)
+            // this will fetch latest order
+            ->defaultSort('created_at', 'desc')
+            ->columns([
+                Tables\Columns\TextColumn::make('id')
+                    ->label('Order ID'),
+                Tables\Columns\TextColumn::make('user.name')
+                    ->label('Customer')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('grand_total')
+                    ->numeric()
+                    ->sortable()
+                    ->money('INR'),
+                TextColumn::make('status')
+                    ->badge()
+                    ->color(fn (string $state) => match ($state) {
+                        'new' => 'info',
+                        'processing' => 'warning',
+                        'shipped' => 'success',
+                        'delivered' => 'success',
+                        'canceled' => 'danger',
+
+                    })
+                    ->icon(fn (string $state) => match ($state) {
+                        'new' => 'heroicon-m-sparkles',
+                        'processing' => 'heroicon-m-arrow-path',
+                        'shipped' => 'heroicon-m-truck',
+                        'delivered' => 'heroicon-m-check-badge',
+                        'canceled' => 'heroicon-m-x-circle',
+                    })
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('payment_method')
+                    ->searchable()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('payment_status')
+                    ->searchable()
+                    ->sortable()
+                    ->badge(),
+                Tables\Columns\TextColumn::make('created_at')
+                    ->label('Order Date')
+                    ->dateTime()
+                    ->sortable(),
+            ])
+            ->actions([
+                Action::make('View Order')
+                    ->url(fn (Order $record): string => OrderResource::getUrl('view', ['record' => $record]))
+                    ->icon('heroicon-o-eye'),
+            ]);
+    }
+}
