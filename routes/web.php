6f5e753f2834341b6f630fa9f9d51fbcefeccab2
megha77777 <meghacute786@gmail.com>
2024-07-06 23:30:21 +0530

    create product detail page

--- routes/web.php
+++ routes/web.php
@@ -2,11 +2,15 @@
 
 use App\Livewire\CartPage;
 use App\Livewire\CategoriesPage;
+use App\Livewire\CheckoutPage;
 use App\Livewire\HomePage;
+use App\Livewire\ProductDetailPage;
 use App\Livewire\ProductsPage;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', HomePage::class);
 Route::get('/categories', CategoriesPage::class);
 Route::get('/products', ProductsPage::class);
+Route::get('/products/{product}', ProductDetailPage::class);
 Route::get('/cart', CartPage::class);
+Route::get('/checkout', CheckoutPage::class);
