a64c5048a8bb55be059e3290c19db21d4945e5e8
megha77777 <meghacute786@gmail.com>
2024-07-07 00:12:00 +0530

    success page

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Livewire\Auth\ForgotPassword;
 use App\Livewire\Auth\Login;
 use App\Livewire\Auth\Register;
 use App\Livewire\Auth\ResetPassword;
+use App\Livewire\CancelPage;
 use App\Livewire\CartPage;
 use App\Livewire\CategoriesPage;
 use App\Livewire\CheckoutPage;
@@ -12,6 +13,7 @@ use App\Livewire\OrderDetailPage;
 use App\Livewire\OrdersPage;
 use App\Livewire\ProductDetailPage;
 use App\Livewire\ProductsPage;
+use App\Livewire\SuccessPage;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', HomePage::class);
@@ -22,7 +24,11 @@ Route::get('/cart', CartPage::class);
 Route::get('/checkout', CheckoutPage::class);
 Route::get('/orders', OrdersPage::class);
 Route::get('/orders/{order}', OrderDetailPage::class);
+
 Route::get('/login', Login::class);
 Route::get('/register', Register::class);
 Route::get('/forgot-password', ForgotPassword::class);
 Route::get('/reset-password', ResetPassword::class);
+
+Route::get('/success', SuccessPage::class);
+Route::get('/cancel', CancelPage::class);
