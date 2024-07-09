5af550801f5ab72f880ccbb2612f4feb478b1e26
megha77777 <meghacute786@gmail.com>
2024-07-09 23:05:04 +0530

    fetch products

--- app/Livewire/ProductsPage.php
+++ app/Livewire/ProductsPage.php
@@ -2,12 +2,28 @@
 
 namespace App\Livewire;
 
+use App\Models\Brand;
+use App\Models\Category;
+use App\Models\Product;
+use Livewire\Attributes\Title;
 use Livewire\Component;
+use Livewire\WithPagination;
 
+#[Title('Products Page | Shopify')]
 class ProductsPage extends Component
 {
+    use WithPagination;
+
     public function render()
     {
-        return view('livewire.products-page');
+        $products = Product::where('is_active', true)->paginate();
+        $brands = Brand::where('is_active', true)->get();
+        $categories = Category::where('is_active', true)->get();
+
+        return view('livewire.products-page', [
+            'products' => $products,
+            'brands' => $brands,
+            'categories' => $categories,
+        ]);
     }
 }
