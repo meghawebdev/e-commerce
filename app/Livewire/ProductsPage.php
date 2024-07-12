792ce4dfee29aa2b215e653a152ff128bedb5592
megha77777 <meghacute786@gmail.com>
2024-07-12 17:23:28 +0530

    filter products

--- app/Livewire/ProductsPage.php
+++ app/Livewire/ProductsPage.php
@@ -6,6 +6,7 @@ use App\Models\Brand;
 use App\Models\Category;
 use App\Models\Product;
 use Livewire\Attributes\Title;
+use Livewire\Attributes\Url;
 use Livewire\Component;
 use Livewire\WithPagination;
 
@@ -14,14 +15,48 @@ class ProductsPage extends Component
 {
     use WithPagination;
 
+    #[Url]
+    public $selected_categories = [];
+
+    #[Url]
+    public $selected_brands = [];
+
+    #[Url]
+    public $featured;
+
+    #[Url]
+    public $on_sale;
+
+    #[Url]
+    public $price_range = 300000;
+
     public function render()
     {
-        $products = Product::where('is_active', true)->paginate();
+        $products = Product::where('is_active', true);
         $brands = Brand::where('is_active', true)->get();
         $categories = Category::where('is_active', true)->get();
 
+        // if array is not empty
+        if (! empty($this->selected_categories)) {
+            $products->whereIn('category_id', $this->selected_categories);
+        }
+
+        if (! empty($this->selected_brands)) {
+            $products->whereIn('brand_id', $this->selected_brands);
+        }
+
+        if (! empty($this->featured)) {
+            $products->where('is_featured', $this->featured);
+        }
+        if (! empty($this->on_sale)) {
+            $products->where('on_sale', $this->on_sale);
+        }
+        if (! empty($this->price_range)) {
+            $products->whereBetween('price', [0, $this->price_range]);
+        }
+
         return view('livewire.products-page', [
-            'products' => $products,
+            'products' => $products->paginate(9),
             'brands' => $brands,
             'categories' => $categories,
         ]);
