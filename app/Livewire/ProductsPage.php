f880385e44a6d5333977f844c55c428a7a81d7e9
megha77777 <meghacute786@gmail.com>
2024-07-13 21:50:10 +0530

    install livewire alert and use in product page

--- app/Livewire/ProductsPage.php
+++ app/Livewire/ProductsPage.php
@@ -7,6 +7,7 @@ use App\Livewire\Partials\Navbar;
 use App\Models\Brand;
 use App\Models\Category;
 use App\Models\Product;
+use Jantinnerezo\LivewireAlert\LivewireAlert;
 use Livewire\Attributes\Title;
 use Livewire\Attributes\Url;
 use Livewire\Component;
@@ -15,6 +16,7 @@ use Livewire\WithPagination;
 #[Title('Products Page | Shopify')]
 class ProductsPage extends Component
 {
+    use LivewireAlert;
     use WithPagination;
 
     #[Url]
@@ -40,6 +42,13 @@ class ProductsPage extends Component
         // dd($product_id);
         $total_count = CartManagement::addItemToCart($product_id);
         $this->dispatch('update-cart-count', total_count: $total_count)->to(Navbar::class);
+
+        $this->alert('success', 'Product added to the cart successfully !', [
+            'position' => 'bottom-end',
+            'timer' => 3000,
+            'toast' => true,
+            'timerProgressBar' => false,
+        ]);
     }
 
     public function render()
--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -16,7 +16,11 @@
         {{ $slot }}
     </main>
     @livewire('partials.footer')
+
     @livewireScripts
+    <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+
+    <x-livewire-alert::scripts />
 </body>
 
 </html>
