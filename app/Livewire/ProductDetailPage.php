4c8d446e55457a9c143ef8a98a2ef82aeaf3690e
megha77777 <meghacute786@gmail.com>
2024-07-13 23:36:19 +0530

    quantity & add to cart functionality in product detail page

--- app/Livewire/ProductDetailPage.php
+++ app/Livewire/ProductDetailPage.php
@@ -2,14 +2,47 @@
 
 namespace App\Livewire;
 
+use App\Helpers\CartManagement;
+use App\Livewire\Partials\Navbar;
 use App\Models\Product;
+use Jantinnerezo\LivewireAlert\LivewireAlert;
 use Livewire\Attributes\Title;
 use Livewire\Component;
 
 #[Title('Product Detail | Shopify')]
 class ProductDetailPage extends Component
 {
+    use LivewireAlert;
+
     public $slug;
+
+    public $quantity = 1;
+
+    public function increaseQty()
+    {
+        $this->quantity++;
+    }
+
+    public function decreaseQty()
+    {
+        if ($this->quantity > 1) {
+            $this->quantity--;
+        }
+    }
+
+    public function addToCart($product_id)
+    {
+        // dd($product_id);
+        $total_count = CartManagement::addItemToCart($product_id);
+        $this->dispatch('update-cart-count', total_count: $total_count)->to(Navbar::class);
+
+        $this->alert('success', 'Product added to the cart successfully !', [
+            'position' => 'bottom-end',
+            'timer' => 3000,
+            'toast' => true,
+            'timerProgressBar' => false,
+        ]);
+    }
 
     public function mount($slug)
     {
