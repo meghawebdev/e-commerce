d563077b4a16567b4a621d10455da2d07be3ef83
megha77777 <meghacute786@gmail.com>
2024-07-17 22:50:44 +0530

    registration

--- app/Livewire/Auth/Register.php
+++ app/Livewire/Auth/Register.php
@@ -2,10 +2,40 @@
 
 namespace App\Livewire\Auth;
 
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Livewire\Attributes\Title;
 use Livewire\Component;
+
+#[Title('Register Page | Shopify')]
 
 class Register extends Component
 {
+    public $name;
+
+    public $email;
+
+    public $password;
+
+    public function save()
+    {
+        // dd($this->name, $this->email, $this->password);
+        $this->validate([
+            'name' => 'required|max:255',
+            'email' => 'required|email|unique:users|max:255',
+            'password' => 'required|min:6|max:255',
+        ]);
+        $user = User::create([
+            'name' => $this->name,
+            'email' => $this->email,
+            'password' => Hash::make($this->password),
+        ]);
+        Auth::login($user);
+
+        return redirect('/');
+    }
+
     public function render()
     {
         return view('livewire.auth.register');
